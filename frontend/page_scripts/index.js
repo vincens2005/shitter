c3441220be1deabdc7343a621bdc2cdc39a14f08
cukmekerb <cukmekerb@gmail.com>
2022-04-19 11:07:02 -0700

    styles for home page

--- frontend/page_scripts/index.js
+++ frontend/page_scripts/index.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		strings: [
 			"The best place for your mental diarrhea",
 			"Censorship is shit, just like your opinions",
-			"Decentralized, just like a sewer",
+			"Decentralized, like a sewer",
 			"Blogging the size of my penis: micro",
 			"Hop in! The water's fine",
 			"Like if the blue bird took a shit on your winshield"
